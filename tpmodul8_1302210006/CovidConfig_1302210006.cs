9a68182fc1cc032f7e79d97d1d8fe07a01418719
Muhammad Fadhil Ardiansyah S <mfadhil661@gmail.com>
2023-04-09 23:44:02 +0700

    menyelesaikan program dengan runtime configuration

---
+++ tpmodul8_1302210006/CovidConfig_1302210006.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul8_1302210006
+{
+
+    internal class CovidConfig_1302210006
+    {
+        public appConfig_1302210006 config;
+        public const string cFile = @"covid_config.json";
+
+        public CovidConfig_1302210006()
+        {
+            try
+            {
+                ReadConfigFile_1302210006();
+                Console.WriteLine("Membaca config file JSON");
+            }
+            catch (FileNotFoundException)
+            {
+
+                Console.WriteLine("Membuat config file default");
+                setDefault_1302210006();
+                WriteConfig_1302210006();
+            }
+        }
+
+        public void ReadConfigFile_1302210006()
+        {
+            string jsontxt = File.ReadAllText(cFile);
+            config = JsonSerializer.Deserialize<appConfig_1302210006>(jsontxt);
+        }
+        public void WriteConfig_1302210006()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            string jsonText = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(cFile, jsonText);
+        }
+
+        public void setDefault_1302210006()
+        {
+            config = new appConfig_1302210006();
+            config.satuan_suhu = "celcius";
+            config.batas_hari_deman = 14;
+            config.pesan_ditolak = "Anda tidak diperbolehkan masuk kedalam gedung ini";
+            config.pesan_diterima = "Anda dipersilahkan untuk masuk kedalam gedung ini";
+        }
+
+
+        public void UbahSatuan()
+        {
+            string ressRead = File.ReadAllText(cFile);
+            config = JsonSerializer.Deserialize<appConfig_1302210006>(ressRead);
+
+            if (config.satuan_suhu.Contains("celcius"))
+            {
+                config.satuan_suhu = "fanrenheit";
+            }
+            else if (config.satuan_suhu.Contains("fanrenheit"))
+            {
+                config.satuan_suhu = "celcius";
+            }
+
+            string newJsonTxt = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true});
+            File.WriteAllText(cFile, newJsonTxt);
+        }
+
+    }
+
+    public class appConfig_1302210006
+    {
+        public string satuan_suhu { set; get; }
+        public int batas_hari_deman { set; get; }
+        public string pesan_ditolak { set; get; }
+        public string pesan_diterima { set; get; }
+
+        public appConfig_1302210006() { }
+        public appConfig_1302210006(string satuan_suhu, int batas_hari_deman, string pesan_ditolak, string pesan_diterima)
+        {
+            this.satuan_suhu = satuan_suhu;
+            this.batas_hari_deman = batas_hari_deman;
+            this.pesan_ditolak = pesan_ditolak;
+            this.pesan_diterima = pesan_diterima;
+        }
+    }
+
+    
+}
--- tpmodul8_1302210006/Program.cs
+++ tpmodul8_1302210006/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul8_1302210006;
+
+CovidConfig_1302210006 con = new CovidConfig_1302210006();
+con.UbahSatuan();
+
+Console.WriteLine("Berapa suhu badan anda saat ini?" + " " + con.config.satuan_suhu);
+string suhuBadan = Console.ReadLine();
+int suhu = (int)Convert.ToInt32(suhuBadan);
+
+Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+string hariDemam = Console.ReadLine();
+int hari = (int)Convert.ToInt32(hariDemam);
+
+if (con.config.satuan_suhu == "celcius")
+{
+    if (suhu > 37.5)
+    {
+        if (hari > con.config.batas_hari_deman)
+        {
+            Console.WriteLine(con.config.pesan_ditolak);
+        }
+        else
+        {
+            Console.WriteLine(con.config.pesan_diterima);
+        }
+    }
+    else {
+        Console.WriteLine(con.config.pesan_diterima);
+    }
+} else if(con.config.satuan_suhu == "fanreinheit")
+{
+    if (suhu > 99.5)
+    {
+        if (hari > con.config.batas_hari_deman)
+        {
+            Console.WriteLine(con.config.pesan_ditolak);
+        }
+        else
+        {
+            Console.WriteLine(con.config.pesan_diterima);
+        }
+    }
+    else {
+        Console.WriteLine(con.config.pesan_diterima);
+    }
+}
+
